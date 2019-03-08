601038f7481c39ff647ec5e4f7f39e9952a49377
Tyler Silva <TylerSilva@student.avrtech.com>
2019-03-08 14:17:02 -0500

    comments

--- assets/code.js
+++ assets/code.js
@@ -1,14 +1,45 @@
+/**
+ * Program: code.js
+ *
+ * Definition: holds all functions used by the HTML
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/7/2019
+ *
+ * History: 3/8/2019 added better comments
+ */
+
+ /**
+ * function: menu
+ *
+ * Definition: opens and closes the menu, as well as changes the url to match.
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/5/2019
+ *
+ * History: 3/7/2019 added keeping the menu open on page change
+ *          3/8/2019 added better comments
+ */
 function menu(addClass, removeClass)
 {
+    //if it's open without an animation, that's only ever used on load, so remove it
     if (document.getElementById("menu").classList.contains("open"))
     {
         document.getElementById("menu").classList.remove("open");
     }
+
+    //if it contains the class it expects the previous animation to be on
     if (document.getElementById("menu").classList.contains(removeClass))
     {
         document.getElementById("menu").classList.remove(removeClass);
     }
+
+    //add the class to start the animation
     document.getElementById("menu").classList.add(addClass);
+
+    // if the menu is open, add that to the url, otherwise, add that it's closed to the url
     if (addClass == "closeAnim")
     {
         history.replaceState(null, null, replace(location.href, "menu=true", "menu=false"));
@@ -19,9 +50,21 @@ function menu(addClass, removeClass)
     }
 
 }
+ /**
+ * function: replace
+ *
+ * Definition: replace or add to the end of a string
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/7/2019
+ *
+ * History: 3/8/2019 added better comments
+ */
 function replace(str, a, b)
 {
     retval = "";
+
     if (str.includes(a))
     {
         retval = str.replace(a, b);
@@ -30,41 +73,86 @@ function replace(str, a, b)
     {
         retval = str + "?" + b;
     }
+
     return retval;
 }
+ /**
+ * function: checkScroll
+ *
+ * Definition: makes the header opaque when the page has scrolled, and tranparent when at the top
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/5/2019
+ *
+ * History: 3/8/2019 added better comments
+ */
 function checkScroll()
 {
     // from https://stackoverflow.com/questions/11373741/detecting-by-how-much-user-has-scrolled
     var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : 
     (document.documentElement || document.body.parentNode || document.body).scrollTop; 
     // my code
+
+    // if not at the top
     if (scrollTop > 1)
     {
+        //make the header opaque
         document.getElementById("header").classList.remove("clearAnim");
         document.getElementById("header").classList.add("opaqueAnim");
     }
     else
     {
+        //make the header transparent
         document.getElementById("header").classList.remove("opaqueAnim");
         document.getElementById("header").classList.add("clearAnim");
     }
 }
-function getUrlVars() {
+ /**
+ * function: getUrlVars
+ *
+ * Definition: this was found online, but it appears to be using a regex 
+ *             expression to convert the url into a dictionary
+ * 
+ * Author: https://html-online.com/articles/get-url-parameters-javascript/
+ *
+ * Date: ???
+ *
+ * History: 3/8/2018 added whitespace
+ */
+function getUrlVars() 
+{
     var vars = {};
+
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
     });
+
     return vars;
 }
+ /**
+ * function: checkUrl
+ *
+ * Definition: if the url says menu = true, start with the menu open
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/7/2019
+ *
+ * History: 3/8/2019 added better comments
+ */
 function checkUrl()
 {
     var url = getUrlVars();
+
     if (url["menu"] != null && url["menu"] == "true")
     {
+
         if (document.getElementById("menu").classList.contains("closeAnim"))
         {
             document.getElementById("menu").classList.remove("closeAnim");
         }
+
         document.getElementById("menu").classList.add("open");
     }
 }
--- assets/sketch.js
+++ assets/sketch.js
@@ -1,13 +1,55 @@
-var corners = [100, -100, -100, 100];
+/**
+ * Program: Sketch.js
+ *
+ * Definition: This is a visual representation of the drive code on the FTC 
+ *             2019 robot. it is used to demonstrate that the robot moves in 
+ *             the correct direction.
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/6/2019
+ *
+ * History: 3/8/2019 added better comments
+ */
 
+//****************************    VARIABLES   *********************************
+var corners = [100, -100, -100, 100];
+var toggle = false;
+var cornersVectors;
+
+/**
+ * function: Setup
+ *
+ * Definition: creates the canvas and puts it in the right place
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/7/2019
+ *
+ * History: 3/8/2019 added better comments
+ */
 function setup() {
   var content = document.getElementById("p5");
   var canv = createCanvas(500, 500)
   canv.parent(content);
 }
+
+/**
+ * function: setmovement
+ *
+ * Definition: copy pasted from the FTC Aztechs 12566 2019 robot. x and y are controller inputs.
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 12/6/2018
+ *
+ * History: 3/7/2019 changed to javascript
+ *          3/8/2019 added better comments
+ */
 function setmovement(x, y)
 {
   //Sets the motor values for directional movement.
+  //this code is just copied from the actual robot.
   var motors = [0, 0, 0, 0, 0];
   if (x >= 0 && y >= 0)
   {
@@ -40,6 +82,17 @@ function setmovement(x, y)
   motors[4] = motors[0] + motors[1] + motors[2] + motors[3];
   return motors;
 }
+/**
+ * function: addAll
+ *
+ * Definition: adds all 4 vectors from each corner.
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/7/2019
+ *
+ * History: 3/8/2019 added better comments
+ */
 function addAll(vectors)
 {
     var retval = createVector(0, 0);
@@ -49,43 +102,60 @@ function addAll(vectors)
     retval.add(vectors[3]);
     return retval;
 }
-var toggle = false;
-var cornersVectors;
+/**
+ * function: draw
+ *
+ * Definition: the code that is run every frame. use inline comments to follow 
+ *             the logic.
+ *
+ * Author: Tyler Silva
+ *
+ * Date: 3/7/2019
+ *
+ * History: 3/8/2019 added better comments
+ */
 function draw() {
-  toggle = !toggle;
+  //sets the correct width and height for the sceen, 20%, or 1/5 of the total window
   height = (windowWidth/5);
   width = height;
+
+  //only resizes the window every other frame, otherwise it is set into an infinite loop
+  toggle = !toggle;
+
   if (toggle)
   {
     resizeCanvas(width, width);
   }
+
+  //paint the background, and make the color of shapes white, without fill
   background(51);
   stroke(255)
   noFill();
+
+  //set the variables x and y to a mapped mouse position, to make (0, 0) the 
+  //center and the numbers not ridiculously large
   var x = -(mouseX-(width/2));
   var y = -(mouseY-(height/2));
   x = map(x, -width/2, width/2, -width/5, width/5)
   y = map(y, -width/2, width/2, -width/5, width/5)
+
+  //get the result from the robot code
   corners = setmovement(x, y);
+
+  //create an array of vectors representing each individual wheel's movement
   cornersVectors = [createVector(corners[0], -corners[0]),
   createVector(corners[1], corners[1]),
   createVector(-corners[2], corners[2]),
   createVector(-corners[3], -corners[3])];
+
+  // create the robot and wheels on screen
   rect(width/10, height/10, width-((width/10)*2), height-((height/10)*2));
-
   line((width/10)*2, (height/10)*2, ((width/10)*2)+corners[0], ((width/10)*2)-corners[0]);
-  
   line(width-((width/10)*2), (height/10)*2, (width-((width/10)*2))+corners[1], ((height/10)*2)+corners[1]);
-  
   line(width-((width/10)*2), height-((height/10)*2), (width-((width/10)*2))-corners[2], (height-((height/10)*2))+corners[2]);
-
   line((width/10)*2, width-((height/10)*2), ((width/10)*2)-corners[3], (width-((height/10)*2))-corners[3]);
 
+  //add all wheel forces together to get the force on the whole robot, and paint it onto the screen
   var finalVector = addAll(cornersVectors);
   line(width/2, height/2, (width/2)+finalVector.x, (height/2)+finalVector.y);
-
-  
-
-  
-  // put drawing code here
 }
